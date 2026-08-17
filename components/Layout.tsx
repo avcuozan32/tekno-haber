1f0dd0efb62fb87d9814b78c8ed4eba5b7818e05
avcuozan32 <avcuozan@gmail.com>
2026-08-17 06:47:56 +0300

    Google Search Console dogrulama eklendi

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,8 +17,8 @@ export default function Layout({
   return (
     <>
       <Head>
-        <title>{title}</title>
 	<meta name="google-site-verification" content="ELayId19pP48Eff8xf23sOiBIravgIxCx5AmajY6A9A" />
+        <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
