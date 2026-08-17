967f50d70c86bcdde671d42a23889cee80ecb452
avcuozan32 <avcuozan@gmail.com>
2026-08-17 14:18:52 +0300

    AdSense site dogrulama kodu eklendi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,14 @@
 export default function Document() {
   return (
     <Html lang="tr">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6220773194970527"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
